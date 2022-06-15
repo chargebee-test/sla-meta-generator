118444a823583d0f724e2e8b1c653c54e6f34e93
cb-christopher <christopher@chargebee.com>
2022-06-15 17:03:16 +0530

    Code review comments

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const yaml = require('js-yaml');
+const fs   = require('fs');
 
 try {
-  const nameToGreet = core.getInput('spec-files');
-  console.log(nameToGreet);
-  const time = (new Date()).toTimeString();
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const specFiles = JSON.parse(core.getInput('spec-files'));
+  specFiles.forEach(file => {
+    const doc = yaml.load(fs.readFileSync(file, 'utf8'));
+    console.log(doc);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
